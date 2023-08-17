0e3ef6032c905e999710205e4b3926cf9293574a
ZoneTwelve <zonetwelve159@gmail.com>
2023-08-17 16:46:50 +0800

    [fix] the UI didn't show up, because tailwind.config.js & uncomment the deleteConversation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./chrome/src/**/*.js', './firefox/src/**/*.js'],
+  content: ['./lib/src/**/*.js'],
   prefix: 'sumz-',
   theme: {
     extend: {
